b0c2d30f6ecc6a78da7085d4830fbba53494a85e
sbx_user1051 <sbx_user1051@169.254.22.93>
2022-09-18 18:34:29 +0000

    Sun Sep 18 18:34:29 UTC 2022

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -2,11 +2,17 @@ package befaster.solutions.SUM;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.security.InvalidParameterException;
+
 @SuppressWarnings("unused")
 public class SumSolution {
 
     public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+        if ( (x < 0 || x > 1000) || (y < 0 || y > 1000)) {
+            throw new InvalidParameterException("Values must be between 0 and 1000!");
+        }
+        return x + y;
     }
 
 }
+
