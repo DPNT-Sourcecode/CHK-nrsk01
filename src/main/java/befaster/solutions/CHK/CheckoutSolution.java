7c58c8b6da89b8b196635450aee3ba2ac5960d8b
sbx_user1051 <sbx_user1051@169.254.22.93>
2022-09-18 18:59:53 +0000

    Sun Sep 18 18:59:53 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,24 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
+import java.io.StringReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CheckoutSolution {
+    
     public Integer checkout(String skus) {
+
         return  -1;
     }
+
+    private Map<String, Integer> groupByProduct(String skus) {
+        char[] basket = skus.toCharArray();
+        Map<String, Integer> groupedProducts = new HashMap<>();
+        for (char product: basket) {
+
+        }
+    }
 }
+
